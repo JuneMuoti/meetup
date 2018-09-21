9941fbf53e557a4ab81d28017ab699f74a7d3d71
June Muoti <june.muoti@gmail.com>
2018-09-21 11:40:14 +0300

    add all

--- meetup/settings.py
+++ meetup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '2ju=8kesns!epy0@!tpimvl$x^io$qtxd4eb_pvrrc)(a76ns5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
